61b836a94d293daa9e2f56c239dda68ffff77e6c
Rafael Porto <rafaeloliveira@portaleducacao.com.br>
2013-07-09 15:55:41 -0400

    Inclusão de trataviva p\ isolamento SNAPSHOT do SQL Server 2008

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -163,6 +163,8 @@ module ActiveRecord
                     FROM [sys].[dm_exec_sessions] 
                     WHERE [session_id] = @@SPID|.squish
             select_value sql, 'SCHEMA'
+	  elsif sqlserver_2008?
+	    "SNAPSHOT"
           else
             user_options['isolation_level']
           end
