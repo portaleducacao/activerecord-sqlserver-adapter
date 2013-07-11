85db8d74842d1c4521e3f8ed356e581fae46516d
Rafael Porto <rafaeloliveira@portaleducacao.com.br>
2013-07-11 16:03:32 -0400

    SQL Server 2008 e tipo de isolamento SNAPSHOT.

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -163,7 +163,7 @@ module ActiveRecord
                     FROM [sys].[dm_exec_sessions] 
                     WHERE [session_id] = @@SPID|.squish
             select_value sql, 'SCHEMA'
-      	  elsif (user_options['isolation_level'] == 'read committed snapshot']) and sqlserver_2008?
+      	  elsif (user_options['isolation_level'] == 'read committed snapshot') and sqlserver_2008?
       	    "SNAPSHOT"
           else
             user_options['isolation_level']
